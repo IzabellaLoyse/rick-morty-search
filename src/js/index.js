c440a7f86bee127d1e9c953dc32f67fc1b975061
Izabella Loyse <izabellaloyse13@gmail.com>
2022-01-01 17:51:36 -0300

    feat: Add funcionalide de buscar personagem

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 /* eslint-disable import/extensions */
 import getDataFromAPI from './service/api.js';
 
+// Returns the total value of characters
 const getTotalCharacters = async () => {
   const totalCharacters = document.querySelector('.js-total-characters');
 
@@ -13,43 +14,78 @@ const getTotalCharacters = async () => {
 
 getTotalCharacters();
 
-const getDataOfCharacter = async () => {
-  const listCharacters = document.querySelector('.js-list-characters');
-
-  const data = await getDataFromAPI('/character/[1,2,3,4,5,7,8,9,10,11,12,13]');
-
+// Insert the list of data about the characters in the HTML
+const insertListIntoHTML = (data, list) => {
   data.forEach((character) => {
     const {
       name, status, species, gender, image, origin,
     } = character;
 
-    listCharacters.insertAdjacentHTML(
+    list.insertAdjacentHTML(
       'beforeend',
       `
-      <li class="c-list-characters__card">
-      <img
-        class="c-list-characters__image"
-        src="${image}"
-        alt="${name}"
-      />
+    <li class="c-list-characters__card">
+    <img
+      class="c-list-characters__image"
+      src="${image}"
+      alt="${name}"
+    />
 
       <article class="c-list-characters__content">
-        <h3 class="js-character-name">${name}</h3>
-        <p class="js-character-status">${status}</p>
+        <h3 class="c-character__name">${name}</h3>
+        <p class="c-character__status js-character-status">${status}</p>
         <p class="js-character-species">${species}</p>
         <p class="js-character-species">${gender}</p>
         <p class="js-character-species">${origin.name}</p>
       </article>
-    </li>
-    `,
+      </li>
+      `,
     );
   });
 };
 
+// Returns a list with the data of 12 characters
+const getDataOfCharacter = async () => {
+  const listCharacters = document.querySelector('.js-list-characters');
+
+  try {
+    const data = await getDataFromAPI(
+      'character/[1,2,3,4,5,7,8,9,10,11,12,13]',
+    );
+
+    insertListIntoHTML(data, listCharacters);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
 getDataOfCharacter();
 
-const input = document.querySelector('.js-search-input');
+// Returns a list with the character data entered in the search field
+const searchCharacter = async () => {
+  const inputSearch = document.querySelector('.js-search-input');
+  const listCharacters = document.querySelector('.js-list-characters');
+  const listSearchCharacter = document.querySelector(
+    '.js-list-search-characters',
+  );
 
-input.addEventListener('keydown', () => {
-  console.log(input.value);
+  try {
+    const { value } = inputSearch;
+
+    const data = await getDataFromAPI(`character/?name=${value}`);
+    const { results } = data;
+
+    if (value !== results.name) listSearchCharacter.textContent = '';
+
+    listCharacters.classList.add('is-list-characters--hidden');
+    listSearchCharacter.classList.remove('is-list-characters--hidden');
+
+    insertListIntoHTML(results, listSearchCharacter);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+document.querySelector('.js-btn-search').addEventListener('click', () => {
+  searchCharacter();
 });
